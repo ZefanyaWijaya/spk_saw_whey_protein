0c7d7736d3017f4ddc48d1738df00f3a527bad74
ZefanyaWijaya <64416388+ZefanyaWijaya@users.noreply.github.com>
2022-04-27 06:53:39 +0700

    updateFe

--- lib/spk_saw_ui.dart
+++ lib/spk_saw_ui.dart
@@ -15,22 +15,22 @@ class _SpkSawUiDashboard extends State<SpkSawUi> {
         children: [
           Container(
             width: 80,
-            color: Colors.red,
+            color: Color.fromRGBO(0, 64, 150, 100),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                SizedBox(height: 30),
+                SizedBox(height: 50),
                 _buildIconDashboardIventory(),
-                
+                SizedBox(height: 50),
+                _buildIconDashboardCalculate(),
+                SizedBox(height: 50),
+                _buildIconDashboardRanking()
               ],
             ),
           ),
           Flexible(
-            child: Column(
-              children: [
-                Text('data'),
-                Text("test")
-              ],
+            child: Container(
+              
             ),
           )
         ],
@@ -50,13 +50,13 @@ class _SpkSawUiDashboard extends State<SpkSawUi> {
           Icon(
             Icons.inventory_2_outlined,
             color: Colors.white,
-            size: 35,
+            size: 30,
           ),
           SizedBox(height: 5),
           Text("List Whey" , style: 
             TextStyle(
               color: Colors.white,
-              fontSize: 16,
+              fontSize: 14,
             ),
           )
         ],
@@ -73,15 +73,40 @@ class _SpkSawUiDashboard extends State<SpkSawUi> {
       child: Column(
         children: [
           Icon(
-            Icons.inventory_2_outlined,
+            Icons.calculate_outlined,
             color: Colors.white,
-            size: 35,
+            size: 30,
           ),
           SizedBox(height: 5),
-          Text("List Whey" , style: 
+          Text("Calculate" , style: 
             TextStyle(
               color: Colors.white,
-              fontSize: 16,
+              fontSize: 14,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIconDashboardRanking () {
+    return InkWell(
+      onTap: () {
+        //call event
+        print("test on tap");
+      },
+      child: Column(
+        children: [
+          Icon(
+            Icons.emoji_events_outlined,
+            color: Colors.white,
+            size: 30,
+          ),
+          SizedBox(height: 5),
+          Text("Ranking" , style: 
+            TextStyle(
+              color: Colors.white,
+              fontSize: 14,
             ),
           )
         ],
