aa49190c0bfe2e7d9063d07ca35a06dc1bd51ae2
ZefanyaWijaya <64416388+ZefanyaWijaya@users.noreply.github.com>
2022-04-28 17:15:54 +0700

    updateTemporaryLayoutHandler>920

--- lib/views/list_whey_protein/list_whey_protein_runner.dart
+++ lib/views/list_whey_protein/list_whey_protein_runner.dart
@@ -24,18 +24,14 @@ class ListWheyProteinRunner extends StatelessWidget {
             child: BlocBuilder<LayoutManagerCubit, LayoutManagerState>(
               bloc: settingsLayout,
               builder: (context, state) {
-                if (state.layoutType != LayoutType.Desktop) {
-                  
-                    return const Scaffold(
-                      body : Center(
-                        heightFactor: 60,
-                        child: Text(
-                          'View not supported for current layout',
-                          style: TextStyle(fontSize: 14),
-                        ),
-                      ),
-                    );
-                  
+                if (state.layoutType != LayoutType.Desktop) { 
+                  return const Center(
+                    heightFactor: 60,
+                    child: Text(
+                      'View not supported for current layout',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  );
                 } else {
                   return Padding(
                     padding: const EdgeInsets.only(left: 48, top: 30),
