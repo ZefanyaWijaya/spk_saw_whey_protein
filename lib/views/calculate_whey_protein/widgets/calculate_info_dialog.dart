650e53a9b2e97a1c0cacd1bf6e893e07014cbc12
ZefanyaWijaya <64416388+ZefanyaWijaya@users.noreply.github.com>
2022-05-19 15:34:24 +0700

    updateDialogCalculate

--- lib/views/calculate_whey_protein/widgets/calculate_info_dialog.dart
+++ lib/views/calculate_whey_protein/widgets/calculate_info_dialog.dart
@@ -12,7 +12,7 @@ class CalculateDialogInfo extends StatelessWidget {
       titlePadding:
       const EdgeInsets.symmetric(horizontal: 48).copyWith(top: 60),
       title: const SizedBox(
-        width: 650,
+        width: 550,
         child: Text(
           "Calculate Simple Additive Weighting Information",
           textAlign: TextAlign.left,
@@ -25,9 +25,9 @@ class CalculateDialogInfo extends StatelessWidget {
         ),
       ),
       contentPadding:
-      const EdgeInsets.symmetric(horizontal: 48).copyWith(top: 30),
+      const EdgeInsets.symmetric(horizontal: 48).copyWith(top: 50),
       content: SizedBox(
-        width: 650,
+        width: 550,
         child: SingleChildScrollView(
           child : Column(
             children: [
@@ -35,23 +35,23 @@ class CalculateDialogInfo extends StatelessWidget {
                 text: const TextSpan(
                   children: <TextSpan> [
                     TextSpan(text: "Proses perhitungan yang dilakukan menggunakan metode SAW, dimana terdapat 5 kriteria yang akan dihitung berdasarkan jenis kriterianya, yakni jenis" ,
-                    style: TextStyle(fontSize: 16 )),
-                    TextSpan(text: " Benefit " , style: TextStyle(fontSize : 16, fontWeight: FontWeight.bold)),
-                    TextSpan(text: "atau" , style:  TextStyle(fontSize: 16)),
-                    TextSpan(text: " Cost ", style: TextStyle(fontSize : 16, fontWeight: FontWeight.bold)),
-                    TextSpan(text: "dan juga berdasarkan bobot dari setiap kriterianya." , style: TextStyle(fontSize: 16)),
+                    style: TextStyle(fontSize: 18 ,)),
+                    TextSpan(text: " Benefit " , style: TextStyle(fontSize : 18, fontWeight: FontWeight.bold)),
+                    TextSpan(text: "atau" , style:  TextStyle(fontSize: 18)),
+                    TextSpan(text: " Cost ", style: TextStyle(fontSize : 18, fontWeight: FontWeight.bold)),
+                    TextSpan(text: "dan juga berdasarkan bobot dari setiap kriterianya." , style: TextStyle(fontSize: 18)),
                   ]
                 )
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 50),
               RichText(
                 text: const TextSpan(
                   children: <TextSpan> [
-                    TextSpan(text: "Benefit " , style: TextStyle(fontSize : 16, fontWeight: FontWeight.bold)),
+                    TextSpan(text: "Benefit " , style: TextStyle(fontSize : 18, fontWeight: FontWeight.bold)),
                     TextSpan(text: "dapat diartikan suatu value asli pada suatu produk yang jumlahnya semakin tinggi semakin baik seperti berat protein (gr) , jumlah rasa yang tersedia, dan kandungan lain dalam whey protein itu sendiri. Sementara" ,
-                    style: TextStyle(fontSize: 16 )),
-                    TextSpan(text: " Cost ", style: TextStyle(fontSize : 16, fontWeight: FontWeight.bold)),
-                    TextSpan(text: "dapat diartikan suatu value asli pada suatu produk yang jumlah nya semakin tinggi maka akan semakin buruk, seperti jumlah harga dan kalori yang terdapat dalam produk - produk yang ada." , style:  TextStyle(fontSize: 16)),
+                    style: TextStyle(fontSize: 18 )),
+                    TextSpan(text: " Cost ", style: TextStyle(fontSize : 18, fontWeight: FontWeight.bold)),
+                    TextSpan(text: "dapat diartikan suatu value asli pada suatu produk yang jumlah nya semakin tinggi maka akan semakin buruk, seperti jumlah harga dan kalori yang terdapat dalam produk - produk yang ada." , style:  TextStyle(fontSize: 18)),
                   ]
                 )
               )
@@ -60,34 +60,28 @@ class CalculateDialogInfo extends StatelessWidget {
         ),
       ),
       actionsPadding: const EdgeInsets.symmetric(horizontal: 48)
-          .copyWith(top: 32, bottom: 40),
+          .copyWith(top: 50, bottom: 40),
       actions: [
-        SizedBox(
+        Container(
+          width: 180,
           height: 45,
-          width: 313,
-          child: Container(
-            width: 180,
-            height: 45,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text(
-                'Close',
-                style: TextStyle(fontSize: 16),
-              ),
-              style: ElevatedButton.styleFrom(
-                elevation: 0,
-                primary: const Color.fromRGBO(2, 106, 199, 1),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                )),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text(
+              'Close',
+              style: TextStyle(fontSize: 16),
             ),
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              primary: const Color.fromRGBO(2, 106, 199, 1),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              )),
           ),
         ),
       ]
     );
   }
-
-  
 }
